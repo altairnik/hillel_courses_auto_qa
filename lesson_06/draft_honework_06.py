cf62803c6c32c146e0d9b0f11f4760845972c047
altairnik <altairnik@gmail.com>
2024-10-05 13:35:29 +0300

    rewrite task 6.1 add comment to task 6.2, 6.3

--- lesson_06/draft_honework_06.py
+++ lesson_06/draft_honework_06.py
@@ -1,28 +1,41 @@
 # task 6.1
+# я неправильно прочитал условие задачи(точнее спутал с задачай 6.2), я почему то думал что код нужно выполнять
+# пока в строке не будет больше 10 уникальных символов поетому и использовал цикл
+# unic_symbol = input('Enter some text\n')
+# while True:
+#     if len(set(unic_symbol)) < 10:
+#         print('There are less than 10 unique symbols')
+#         unic_symbol = input('Enter some text\n')
+#     else:
+#         print(f'There are {len(set(unic_symbol))} unique symbols')
+#         break
 unic_symbol = input('Enter some text\n')
-for _ in unic_symbol:
-    if len(unic_symbol) <= 9:
-        print('False')
-        break
-    elif len(unic_symbol) - len(set(unic_symbol)) <= 10:
-        print('True')
-        break
+if len(set(unic_symbol)) < 10:
+    print('False')
+else:
+    print(f'There are {len(set(unic_symbol))} unique symbols')
 
 # task 6.2
 # тут если в инпуте будет "H" и "h" то постоянно будет выводится первый принт так как первая поверка идет на "h"
 while True:
-    checked_text = input('Enter some text\n')
-    if checked_text.find('h') != -1:
-        print('Congratulations, your find "h"')
+    some_text = input('Enter some text\n') # етот вариант намного проще
+    if 'h' in some_text or 'H' in some_text:
+        print('True')
         break
-    elif checked_text.find('H') != -1:
-        print('Congratulations, your find "H"')
-        break
+# while True:
+#     checked_text = input('Enter some text\n')
+#     if checked_text.find('h') != -1:
+#         print('Congratulations, your find "h"')
+#         break
+#     elif checked_text.find('H') != -1:
+#         print('Congratulations, your find "H"')
+#         break
 
 # task 6.3
 lst1 = ['1', '2', 3, True, 'False', 5, '6', 7, 8, 'Python', 9, 0, 'Lorem Ipsum']
 lst2 = []
 for element in lst1:
+# lst2 = [k for k in lst1 if type(k) is str] # как действительно проще
     if type(element) == str:
         lst2.append(element)
 print(lst2)
